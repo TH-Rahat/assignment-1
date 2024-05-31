38471d0b42b2e7d7fdc7ea1fef99ea032cf28d99
TH-Rahat <1foxgameing@gmail.com>
2024-05-31 22:47:52 +0600

    first commit

---
+++ [name].js
@@ -0,0 +1,60 @@
+// 1
+let carName = "Volvo";
+console.log(carName);
+// 2
+let firstName = "John";
+let lastName = "Doe";
+let age = 35;
+// 3
+x = 10;
+y = 5;
+x = x * y
+console.log(x)
+// 4
+let length = 16; // data type: Number
+let lastNames = "Johnson"; // data type: String
+
+const z = {
+  firstName: "John",  
+  lastNames: "son"
+}; // data type: Object
+
+// 5
+function myFunction() {
+    console.log("Hello World!");
+}
+myFunction();
+
+// 6
+// 6 in html
+
+// 7
+// 7 in html
+
+// 8
+const cars = ["Volvo", "Jeep", "Mercedes"];
+console.log(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"];
+Brand[0] = "Ford";
+console.log(Brand);
+
+// 9
+let randomNumber = Math.random();
+console.log(randomNumber);
+
+let largestNumber = Math.max(10, 20);
+console.log(largestNumber);
+
+let squareRoot = Math.sqrt(9);
+console.log(squareRoot);
+
+// 10
+let xx = 10;
+let yy = 5;
+console.log(xx > yy);
+
+let ages = 20;
+let message = ages < 18 ? "Too young" : "Old enough";
+
+console.log(message);
